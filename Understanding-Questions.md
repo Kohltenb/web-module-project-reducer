11fd6325a3fc01ee8d1cf2c1d13d48c2c76cf553
Kohlten Bills <kohltenbillsy@gmail.com>
2022-06-15 18:27:16 -0600

    complete with calculator

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,13 @@
 # Understanding Questions:
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
-* 
+* the event handler runs
+* invoking dispatch
+* executing addone function 
+* which executes the reducer
+* inputing type ADD_ONE 
+* switch method then executes its return of state + 1
+
 ...
 
 * TotalDisplay shows the total plus 1.
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,40 @@
-import React from 'react';
-
+import React, {useReducer} from 'react';
 import './App.css';
 
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
+import reducer, { initialState } from '../reducers';
+// import { addOne } from '../actions';
+import { applyNumber } from '../actions'
+import { changeOperation} from '../actions'
+import { clearDisplay } from '../actions'
+import { memoryAdd } from '../actions'
+import { addingToMemory} from '../actions'
+import { clearMemory } from '../actions'
+
+
 function App() {
-  return (
+  const [ state, dispatch ] = useReducer(reducer, initialState) 
+  const handleClick = (number) => {
+    dispatch(applyNumber(number))
+  }
+  const handleOperator = (operator) => {
+    dispatch(changeOperation(operator))
+  }
+  const handleClear = () => {
+    dispatch(clearDisplay())
+  }
+  const handleMemory = () => {
+    dispatch(memoryAdd())
+  }
+  const handleAddingToMemory = () => {
+    dispatch(addingToMemory())
+  }
+  const handleClearMemory = () => {
+    dispatch(clearMemory())
+  }
+  return ( 
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
         <a className="navbar-brand" href="#"> Reducer Challenge</a>
@@ -16,44 +44,44 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b>{state.operation}</span>
+              <span id="memory"><b>Memory:</b>{state.memory}</span>
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton value={"M+"} onClick={handleMemory}/>
+              <CalcButton value={"MR"} onClick={handleAddingToMemory} />
+              <CalcButton value={"MC"} onClick={handleClearMemory}/>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={() => handleClick(1)}/>
+              <CalcButton value={2} onClick={() => handleClick(2)}/>
+              <CalcButton value={3} onClick={() => handleClick(3)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={() => handleClick(4)}/>
+              <CalcButton value={5} onClick={() => handleClick(5)}/>
+              <CalcButton value={6} onClick={() => handleClick(6)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={() => handleClick(7)}/>
+              <CalcButton value={8} onClick={() => handleClick(8)}/>
+              <CalcButton value={9} onClick={() => handleClick(9)}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton value={"+"} onClick={() => handleOperator('+')}/>
+              <CalcButton value={"*"} onClick={() => handleOperator('*')}/>
+              <CalcButton value={"-"} onClick={() => handleOperator('-')}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton value={"CE"} onClick={handleClear}/>
             </div>
 
           </form>
